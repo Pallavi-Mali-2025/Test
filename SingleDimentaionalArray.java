04809c8f0e057544f99469630ea3b3945580197d
PallaviMali24 <malipallavi586@gmail.com>
2025-07-30 13:25:21 +0530

    Initial commit of Java projects

---
+++ SingleDimentaionalArray.java
@@ -0,0 +1,30 @@
+package arrayprograms;
+
+public class SingleDimentaionalArray {
+
+	public static void main(String[] args) {
+		
+      int a[] = new int [5];
+      a[0] =100 ;
+      a[1] =200 ;
+      a[2] =300 ;
+      a[3] = 400;
+      a[4] =500 ;
+//      System.out.println(a[2]);
+//      System.out.println(a.length);
+//      for (int i=0;i<=a.length-1;i++)
+//      {
+//      System.out.println(a[i]);
+//      }
+      for (int p:a)
+      
+      {
+    	  System.out.println( p);
+      }
+      
+      
+      
+         		  
+}
+
+}
